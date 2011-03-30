fe6bc100e39ca4eed7946b6bd6a2171924272515
Roberto Vasquez Angel <robotex@robotex.de>
2011-03-30 15:32:56 +0200

    Initial commit

---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = 'e834d90eadc7075c973318b4d5093b907fe9fb15754aff272c22feb861cbd37eacbf4e65e4dbdf9565d86e4564d805646218c106fa78e9cb7e89217b8d64048e'
---
+++ test/integration/navigation_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class NavigationTest < ActiveSupport::IntegrationCase
+  test 'pdf request sends a pdf file' do
+    visit home_path
+    click_link 'PDF'
+    
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"',
+                 headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+    assert_match /Prawn/, page.body        
+  end
+  
+  test 'pdf renderer uses the specified template' do
+    visit '/another.pdf'
+    assert_equal 'binary', headers['Content-Transfer-Encoding']
+    assert_equal 'attachment; filename="contents.pdf"',
+                 headers['Content-Disposition']
+    assert_equal 'application/pdf', headers['Content-Type']
+    assert_match /Prawn/, page.body             
+  end
+  
+  protected
+    def headers
+      page.response_headers
+    end  
+end
